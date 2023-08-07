c9389b79393a5fdd45d572b3828c4034067daf0f
HuynhKhanh1402 <khanh@duck.com>
2023-08-07 11:12:12 +0700

    Initial commit

---
+++ src/main/java/dev/khanh/testlicense/Test_license.java
@@ -0,0 +1,17 @@
+package dev.khanh.testlicense;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Test_license extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
